0f459f8fb1d5b4a6d723e95de4313aa41a5a38e3
jianzhao16 <jzhao16@students.desu.edu>
2022-08-07 19:42:28 -0400

    08072022

--- Run-BF-C2DL-HSC-01.sh
+++ Run-BF-C2DL-HSC-01.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # /home/users/jzhao/app/anaconda3/envs/UVA-NL/bin/python -u siam_track.py --dataset_path="../BF-C2DL-HSC" --sequence=1 --siamese_path="model_weights/siam_model.pth" -get_initial_seg_from_lux
- /home/users/jzhao/app/anaconda3/envs/UVA-NL/bin/python -u siam_track.py --dataset_path="../BF-C2DL-HSC" --sequence=1  >runlog.out 2>&1 </dev/null &
-
+# /home/users/jzhao/app/anaconda3/envs/UVA-NL/bin/python -u siam_track.py --dataset_path="../BF-C2DL-HSC" --sequence=1  >runlog.out 2>&1 </dev/null &
+ ~/anaconda3/envs/UVA-NL-V6/bin/python -u siam_track.py --dataset_path="../BF-C2DL-HSC" --sequence=1
